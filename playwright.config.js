5697e17232a047311f47125a0819175f89ef8894
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-10-14 09:18:32 -0500

    Configue the base url for coduit and the reporter options

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ const config = {
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: "html",
+    reporter: [["html", { open: "never" }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -40,12 +40,8 @@ const config = {
         // baseURL: 'http://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: "on-first-retry",
-        baseURL: "https://restful-booker.herokuapp.com",
-        extraHTTPHeaders: {
-            Accept: "application/json",
-            Content_Type: "application/json",
-        },
+        trace: "retain-on-failure",
+        baseURL: "https://demo.realworld.io/",
     },
 
     /* Configure projects for major browsers */
