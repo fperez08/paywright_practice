798b20b7c02262b6bb676b6d58ccbbd3af1cdff9
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-10-17 12:12:09 -0500

    Page object model for login page

--- src/pages/login_page.js
+++ src/pages/login_page.js
@@ -11,4 +11,23 @@ exports.LoginPage = class LoginPage {
         this.content = userHomeContent;
         this.path = "#/login";
     }
+
+    /**
+     * Navigate to the sig in page.
+     */
+    async navigate() {
+        this.content.page.goto(this.path);
+    }
+
+    /**
+     * Log in with the given credentials.
+     *
+     * @param {string} email - User email.
+     * @param {string} password - User password.
+     */
+    async login(email, password) {
+        await this.content.emailInput.fill(email);
+        await this.content.passwordInput.fill(password);
+        await this.content.signInButton.click();
+    }
 };
