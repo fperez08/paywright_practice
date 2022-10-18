54d28349d8a660b4802fd473115305e24ff35717
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-10-18 18:10:46 -0500

    Model and js doc configuration

---
+++ src/common/base/models.js
@@ -0,0 +1,14 @@
+/**
+ * Object models
+ * @module base/models
+ */
+
+/**
+ * User model.
+ *
+ * @typedef {object} User
+ *
+ * @property {string} user_name Account user name.
+ * @property {string} email User email.
+ * @property {string} password User password.
+ */
